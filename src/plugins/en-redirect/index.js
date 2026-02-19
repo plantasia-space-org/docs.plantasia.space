f66ffd497ee3cb1c969194fda3aaa9cb79039f15
maar34 <mar@headbrothers.com>
2026-02-19 14:47:40 +0000

    feat: Enhance English redirect plugin to check current locale before adding routes

--- src/plugins/en-redirect/index.js
+++ src/plugins/en-redirect/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
-export default function enRedirectPlugin() {
+export default function enRedirectPlugin(context) {
+  const currentLocale = context?.i18n?.currentLocale;
+
   return {
     name: 'en-redirect',
     async contentLoaded({ actions }) {
+      if (currentLocale !== 'en') {
+        return;
+      }
+
       const { addRoute } = actions;
 
       addRoute({
